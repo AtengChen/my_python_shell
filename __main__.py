57cc5bab6e72b64e75fd9176934cd260aeb56d11
Aten Chen <109785614+AtengChen@users.noreply.github.com>
2023-06-21 15:01:26 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@
 __author__ = "Aten Chen"
 __email__ = ("ateng0721@hotmail.com", "9944560@schoolsnet.act.edu.au")
 __doc__ = open(".\README.md", "r").read()
-__version__ = "0.6"
+__version__ = "0.7"
 
 delattr(__import__(__name__), "__file__")
 setattr(__import__(__name__), "__loader__", None)

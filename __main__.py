8b4eb789e6816a8ead4caddf91acc1a704177e9d
Aten Chen <109785614+AtengChen@users.noreply.github.com>
2023-08-12 16:02:12 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,22 +1,24 @@
 #! python3
 
+import os.path
+
 __author__ = "Aten Chen"
 __email__ = ("ateng0721@hotmail.com", "9944560@schoolsnet.act.edu.au")
-__doc__ = open(".\README.md", "r", encoding="UTF-8").read()
+__doc__ = open(os.path.dirname(__file__) + "\\README.md", "r", encoding="UTF-8").read()
 __version__ = "0.8"
 
-import __init__
-
-get_my_python_shell = __init__
-
 if __name__ == "__main__":
-    import argparse, sys
+    import argparse, sys, __init__
+
+    get_my_python_shell = __init__
+
     parser = argparse.ArgumentParser(description='My Python Shell')
     parser.add_argument("-d", "--debug", help="Debugging mode", action='store_true')
     parser.add_argument("--noprettytb", help="Formatted traceback", action='store_true')
     parser.add_argument("-a", "--ascii", help="Enable ascii charactars", action='store_true')
     parser.add_argument("--nocopyresult", help="Don't copy the result of a expression after evaluating it", action='store_true')
     parser.add_argument("--nocolor", help="No color display", action='store_true')
+    parser.add_argument("--nodetailerr", help="No detailed error", action='store_true')
     
 
     args = parser.parse_args()
@@ -26,11 +28,13 @@ if __name__ == "__main__":
     __init__.config["enable_ascii"] = args.ascii
     __init__.config["copy_result"] = not args.nocopyresult
     __init__.config["nocolor"] = args.nocolor
+    __init__.config["detail_err"] = not args.nodetailerr
     
     del argparse, __init__, args, parser
 
     delattr(__import__(__name__), "__file__")
     setattr(__import__(__name__), "__loader__", None)
+    setattr(__import__(__name__), "__spec__", None)
     
     get_my_python_shell.init()
     sys.exit(get_my_python_shell.main())
